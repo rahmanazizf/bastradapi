036a3266d289ce2e98b16327d71c8a8fc3ae0ddb
rahmanazizf <rahmanazizf@gmail.com>
2023-11-11 10:39:58 +0700

    on progress: authorization

--- middleware/authorization.go
+++ middleware/authorization.go
@@ -1,1 +1,22 @@
 package middleware
+
+import "github.com/gin-gonic/gin"
+
+// function to access product
+func AuthorizedProduct() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		// access params
+
+		// access database
+
+		// get claims from authentication process
+
+		// query data
+
+		// match claims with the data had been queried
+
+		// if the data is matched with claims, return the data
+
+		// else, abort
+	}
+}
