775725289ea9566cce9188f8569154807294a703
rahmanazizf <rahmanazizf@gmail.com>
2023-11-26 19:39:17 +0700

    authorization function done

--- middleware/authorization.go
+++ middleware/authorization.go
@@ -6,30 +6,47 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	jwt5 "github.com/golang-jwt/jwt/v5"
 )
 
 // function to access product
-func AuthorizedProduct() gin.HandlerFunc {
+func AuthorizeProduct() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var product *models.Product
-		// access params
-		userData, exists := ctx.Get("userData")
-		if !exists {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"message": "You are not an authorized user!",
-				"data":    userData, // TO DELETE
+		// claim from authentication process
+		adminData := ctx.MustGet("AdminData").(jwt5.MapClaims)
+		adminID := uint(adminData["id"].(float64))
+		// access database
+		res := database.ConnectToDB().Find(&product, "uuid = ?", ctx.Param("productUUID"))
+		if res.Error != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"status":  "failed",
+				"message": res.Error.Error(),
 			})
 		}
-		// access database
-		database.ConnectToDB().Find(&product)
-		// get claims from authentication process
-
+		if res.RowsAffected == 0 {
+			ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"status":  "failed",
+				"message": "No records with given uuid found",
+			})
+			return
+		}
 		// query data
-
-		// match claims with the data had been queried
-
+		if product.AdminID != int(adminID) {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"status":  "failed",
+				"message": "You are unauthorized",
+			})
+			return
+		}
 		// if the data is matched with claims, return the data
-
+		ctx.Next()
 		// else, abort
+	}
+}
+
+func AuthorizeProductCreation() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
 	}
 }
